ca5770faf3e064afb941f99ad66991289264f683
jadeeg <jadeegoncalves@gmail.com>
2025-03-21 19:23:41 -0300

    json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+ 
 
+ const key = "40294d271t694ab0b8549a4358o9f5f1";
+ const prompt= "form.value";
+const apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  function poems (event) {
+    event.preventDefault();
+   axios.get(apiUrl).then((response) => {
+     console.log(response.data);
+   });
+  }
+
+   let form  = document.querySelector("#form");
+   form.addEventListener("submit", poems);
